2cd0a451aa4624e4ca6ce9ed669b2491d4841662
Sally Cheng <sacheng@hmc.edu>
2016-03-21 23:12:12 -0700

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,7 +2,9 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+        String version = System.getProperty("java.specification.version");
+        Double versionNum = Double.parseDouble(version);
+        return versionNum;
     }
 
     public static void main(String[] args) {
